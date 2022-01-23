42caf1ddbbdcb210eead4f2c17957e6346da9cf1
serhii.nastenko <serhii.nastenko@nentgroup.com>
2022-01-24 01:13:54 +0200

    base layout

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
+import Directory from "../../components/directory/directory.component";
+import './homepage.styles.scss';
 
-const Homepage = () => {
-    <div className={'homepage'}>
-        <div className={'directory-menu'}>
-            <div className={'menu-item'}>
-                <div className={'content'}>
-                    <h1 className={'title'}>HATS</h1>
-                    <span className={'subtitle'}>SHOP NOW</span>
-                </div>
-            </div>
-        </div>
+const HomePage = () => (
+    <div className='homepage'>
+        <h1>Welcome to my Homepage</h1>
+        <Directory />
     </div>
-}
+)
+
+export default HomePage;
