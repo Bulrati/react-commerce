870b55fb05ba9168ecf2bda034a6315eb49fccf6
serhii.nastenko <serhii.nastenko@nentgroup.com>
2022-02-13 00:45:28 +0200

    Styling for directory and menu-item components

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './menu-item.styles.scss'
 
-export const MenuItem = ({title, imageUrl}) => (
-  <div className={'menu-item'}>
-      <div className={'content'}>
+export const MenuItem = ({title, imageUrl, size}) => (
+  <div style={{
+      backgroundImage: `url(${imageUrl})`
+  }} className={`${size} menu-item`}>
+      <div className='content'>
           <h1 className={'title'}>{title}</h1>
           <span className={'subtitle'}>SHOP NOW</span>
       </div>
